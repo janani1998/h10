04ef00b451602edddff2a7f737d98cfd534cff1b
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-20 15:27:15 +0530

    Create concatenate of the number.java

---
+++ concatenate of the number.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int m=in.nextInt();
+		String s=String.valueOf(n)+String.valueOf(m);
+		System.out.print(s);
+		
+	}
+}
